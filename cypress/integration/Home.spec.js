4869fbe1b6a873775eac3fdba1d3fd8e5414387b
Constantin <constantingoeldel@gmail.com>
2021-06-11 08:37:08 +0300

    adds cypress ci

---
+++ cypress/integration/Home.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe("Home Screen", () => {
+    beforeEach("visits site", () => {
+        cy.visit("/")
+    })
+    it("Displays the slogan", () => {
+        cy.contains("Madagascars peer to peer e-commerce platform")
+    })
+})
