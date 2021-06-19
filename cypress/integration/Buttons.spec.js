0df88d422e77c87dc27ef65eb8a901497e706c32
Jacquit <jacquit.aja@onja.org>
2021-06-19 15:24:04 +0300

    Fix(Buttons): Resolution of the Buttons location, renewed the links in the intergration, moving the fonts into the fonts folder

--- cypress/integration/Buttons.spec.js
+++ cypress/integration/Buttons.spec.js
@@ -8,21 +8,21 @@ describe('Buttons Screen', () => {
   it('Displays the learn button', () => {
     cy.contains('Buttons')
     cy.visit(
-      'http://localhost:6006/iframe.html?id=buttons--learn-abled&args=&viewMode=story'
+      'http://localhost:6006/iframe.html?id=components-buttons--learn-abled&args=&viewMode=story'
     )
     cy.contains('Learn more')
   })
   it('Displays the learn button', () => {
     cy.contains('Buttons')
     cy.visit(
-      'http://localhost:6006/iframe.html?id=buttons--learn-disabled&args=&viewMode=story'
+      'http://localhost:6006/?path=/story/components-buttons--learn-disabled'
     )
     cy.contains('Learn more')
   })
   it('Displays the trust button', () => {
     cy.contains('Buttons')
     cy.visit(
-      'http://localhost:6006/iframe.html?id=buttons--trust&args=&viewMode=story'
+      'http://localhost:6006/iframe.html?id=components-buttons--trust&args=&viewMode=story'
     )
     cy.contains('Can you trust us?')
   })
