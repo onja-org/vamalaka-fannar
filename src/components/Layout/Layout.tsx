c6d3b5fb3674d9444e7807fe265113cbb337c582
Peta Jeannie <peta.jea@onja.com>
2021-06-22 17:48:43 +0300

    Fix(Fix layout): Deleted layout wrapper on Header and Footer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import styled from 'styled-components'
 
 export interface LayoutProps {
   children: JSX.Element
 }
 
-export const Layout: React.FC<LayoutProps> = ({children}: LayoutProps) => {
-  return (
-    <Wrapper>{children}</Wrapper>
-  )
+export const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
+  return <>{children}</>
 }
-
-const Wrapper = styled.div`
-margin:0; 
-padding: 0;
-`
