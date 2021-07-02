f4d88fae2fab131433272927036367eb588ad525
Jacquit <jacquit.aja@onja.org>
2021-07-02 11:51:11 +0300

    Fix(buttons): changing the position relatives into position absolute

--- src/components/Buttons/Buttons.stories.tsx
+++ src/components/Buttons/Buttons.stories.tsx
@@ -1,24 +1,26 @@
 import { Story, Meta } from '@storybook/react'
 import Buttons from './Buttons'
 import { ButtonsProps } from './Buttons'
+
 export default {
   title: 'Components/Buttons',
   component: Buttons,
 } as Meta
+
 const Button: Story<ButtonsProps> = (args) => <Buttons {...args} />
-export const LearnAbled = Button.bind({})
-LearnAbled.args = {
-  learnabled: true,
+export const LearnEnabled = Button.bind({})
+LearnEnabled.args = {
+  learnEnabled: true,
   label: 'Learn more',
 }
-export const LearnDisabled = Button.bind({})
-LearnDisabled.args = {
+export const Learndisabled = Button.bind({})
+Learndisabled.args = {
   learnabled: true,
   disabled: true,
   label: 'Learn more',
 }
 export const Trust = Button.bind({})
 Trust.args = {
-  learnabled: false,
+  learnEnabled: false,
   label: 'Can you trust us?',
 }
