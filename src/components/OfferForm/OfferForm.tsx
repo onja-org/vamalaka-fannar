7d46d10253d9af581e15d00068aea67992c4d9a9
mitia-anah <rosny.rat@onja.org>
2022-02-24 09:39:23 +0300

    fixed errors

---
+++ src/components/OfferForm/OfferForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+function OfferComponent() {
+    return (
+
+        <div></div>
+    )
+}
--- src/containers/RegisterAccountContainer.tsx
+++ src/containers/RegisterAccountContainer.tsx
@@ -72,21 +72,21 @@ export const RegisterAccount = () => {
   }
 
   const usernameErrorMessage =
-    userError?.message?.toString()?.indexOf('CODE:EU2') > -1
+    userError?.message?.toString()?.indexOf('CODE:EU2')
       ? 'User name already exists'
       : ''
   const emailErrorMessage =
-    userError?.message?.toString()?.indexOf('CODE:EU6') >= 0
+    userError?.message?.toString()?.indexOf('CODE:EU6')
       ? 'Email must be a valid email'
       : ''
 
   const passwordErrorMessage =
-    userError?.message?.toString()?.indexOf('CODE:EU7') >= 0
+    userError?.message?.toString()?.indexOf('CODE:EU7')
       ? 'Password must be at least 8 characters long'
       : ''
 
   const confirmPasswordErrorMessage =
-    userError?.message?.toString()?.indexOf('CODE:EU8') >= 0
+    userError?.message?.toString()?.indexOf('CODE:EU8')
       ? "Passwords doesn't match"
       : ''
 
