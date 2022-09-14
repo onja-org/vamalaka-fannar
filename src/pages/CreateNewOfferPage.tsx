0747a405c076b7eca39f7c684093fbe061f05fb4
Belle Sugi <sugi.zaf@onja.org>
2022-09-14 09:13:54 +0300

    New offer

--- src/pages/CreateNewOfferPage.tsx
+++ src/pages/CreateNewOfferPage.tsx
@@ -6,7 +6,6 @@ import Input from "../components/Input/Input";
 import { fonts } from "../globalStyles/fonts";
 import { useSelector } from 'react-redux'
 import { categoriesSelector, fetchCategories } from '../redux/slices/categoriesSlice'
-import {fetchUserOffers} from '../redux/slices/userOfferSlice'
 import { useEffect } from 'react'
 import { CURRENCIES_DROP_DOWN_OPTIONS, UNIT_DROP_DOWN_OPTIONS } from '../constants'
 import { useAppDispatch } from '../redux/hooks'
@@ -14,8 +13,10 @@ import { selectUpdateAdError } from "../redux/slices/offerByIdSlice";
 import { ErrorMessage } from "../components/ErrorMessage/ErrorMessage"
 import { UploadFile } from "../components/UploadFile/UploadFile";
 import { ThumbnailGrid } from "../components/ThumbnailGrid/ThumbnailGrid";
-import { event } from "cypress/types/jquery";
-import { any } from "cypress/types/bluebird";
+import {fetchCreateNewOffer} from "../redux/slices/userOfferSlice"
+
+
+
 // import { BACKEND_URL } from "../localhostURL";
 
 export interface NewFormProps {
@@ -115,9 +116,10 @@ export const CreateNewOffer = (text) => {
 
   const [newOffer, setNewOffers] = React.useState(formData);
   
- 
-
   const setTitle = ({ target }) => { setNewOffers({ ...newOffer, title: target.value }) }
+  
+
+
   const setDescription = ({ target }) => { setNewOffers({ ...newOffer, body: target.value }); }
   const setAmountOfProduct = ({ target }) => { setNewOffers({ ...newOffer, amountOfProduct: target.value }) }
   const setPrice = ({ target }) => { setNewOffers({ ...newOffer, price: target.value }) }
@@ -143,23 +145,24 @@ export const CreateNewOffer = (text) => {
 
   }, [newOffer.photos])
 
+ 
   const submitNewOffer = React.useCallback(
+    
     (event: React.MouseEvent<Element, MouseEvent>) => {
       event?.preventDefault()
       dispatch(
-        fetchUserOffers({ 
+        fetchCreateNewOffer({
           id: '',
-          title: event.currentTarget.title.value,
-          photos: [] as any[],
-          body: event.currentTarget.body.value,
-          amountOfProduct: event.currentTarget.amountOfProduct.value,
-          price: event.currentTarget.price.value,
-          unit: event.currentTarget.unit.value,
-          currency: event.currentTarget.currency.value,
-          category: {
-            title: event.currentTarget.title.value,
-            id: event.currentTarget.id.value,
-          }
+          title: (event.currentTarget as any).title.value,
+          photos: {url: '',
+            info: '',
+            isPrimary: false},
+          body: (event.currentTarget as any).body.value,
+          amountOfProduct: (event.currentTarget as any).amountOfProduct.value,
+          price: (event.currentTarget as any).price.value,
+          unit: (event.currentTarget as any).unit.value,
+          currency: (event.currentTarget as any).currency.value,
+          categoryId: (event.currentTarget as any).categoryId.value,
         })
       )
     },
@@ -230,7 +233,7 @@ const checkIfOnlyImageAsignStar = (imageThumbnails) => {
     <Wrapper>
       <WrapperEditOffer>
         <HeaderEditOffer>Create New Offer</HeaderEditOffer>
-        <Form>
+        <Form  onSubmit={submitNewOffer as any}>
           <FormEditDetail>
             <Input
               label={'Product name*'}
@@ -290,13 +293,8 @@ const checkIfOnlyImageAsignStar = (imageThumbnails) => {
           </FormEditDetail>
           {offerUpdateAdError && <ErrorMessage message={offerUpdateAdError.message} />}
           <WrapperButton>
-            {/* <Button
-              label={'Save changes'}
-              isPrimary={true}
-              type={'submit'}
-              onClick={(e) => submitNewOffer(e)}
-            /> */}
-            <Button icon={''} type="button" onClick={(e) =>  submitNewOffer(e)} label="Create new" />
+            {/* <Button icon={''} type="button" onClick={(e) =>  submitNewOffer(e)} label="Create new" /> */}
+            <Button icon={''} type="submit" label="Create new" />
           </WrapperButton>
         </Form>
       </WrapperEditOffer>
